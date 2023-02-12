a9c62c9c2621b4f413b0d9bc8326b39b526c915a
prokhor-andrew <prokhor.andrew@gmail.com>
2023-02-12 14:32:51 -0500

    Made features hashable

--- Sources/simprokstate/Feature.swift
+++ Sources/simprokstate/Feature.swift
@@ -136,3 +136,14 @@ public extension Machine {
         }
     }
 }
+
+
+extension Feature: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(machines)
+    }
+
+    public static func ==(lhs: Feature<IntTrigger, IntEffect, ExtTrigger, ExtEffect>, rhs: Feature<IntTrigger, IntEffect, ExtTrigger, ExtEffect>) -> Bool {
+        lhs.machines == rhs.machines
+    }
+}
