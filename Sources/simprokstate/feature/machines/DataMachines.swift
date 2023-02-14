44ae0eccd5364a897e8e349e78863149d72ad839
prokhor-andrew <prokhor.andrew@gmail.com>
2023-02-14 13:19:18 -0500

    Added DataMachine FeatureMachines

---
+++ Sources/simprokstate/feature/machines/DataMachines.swift
@@ -0,0 +1,20 @@
+//
+// Created by Andriy Prokhorenko on 14.02.2023.
+//
+
+import simprokmachine
+
+public struct DataMachines<Data, Trigger, Effect>: FeatureMachines {
+
+    public let data: Data
+    public let machines: Set<Machine<Effect, Trigger>>
+
+    public init(_ data: Data, machines: Set<Machine<Effect, Trigger>>) {
+        self.data = data
+        self.machines = machines
+    }
+
+    public init(_ data: Data, machines: Machine<Effect, Trigger>...) {
+        self.init(data, machines: Set(machines))
+    }
+}
